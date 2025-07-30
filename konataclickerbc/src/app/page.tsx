4ecb628cba6608e967f41c44812b43dd7e075b9d
Shawn Huang <69904687+Mutton9558@users.noreply.github.com>
2025-07-30 11:47:37 +0800

    transaction in frontend instead of backend (backend now useless)

--- konataclickerbc/src/app/page.tsx
+++ konataclickerbc/src/app/page.tsx
@@ -1,8 +1,84 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import { ethers } from "ethers";
+import { useEffect, useState, useRef } from "react";
+import { ethers, BrowserProvider, JsonRpcSigner } from "ethers";
 import "./style.css";
+import { Contract } from "ethers";
+
+const ABI = [
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "user",
+        type: "address",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "score",
+        type: "uint256",
+      },
+    ],
+    name: "ScoreSaved",
+    type: "event",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "user",
+        type: "address",
+      },
+    ],
+    name: "getData",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    name: "scoreData",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "data",
+        type: "uint256",
+      },
+    ],
+    name: "setData",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+];
+
+const contractAddr = process.env.NEXT_PUBLIC_CONTRACT_ADDRESS;
 
 declare global {
   interface Window {
@@ -12,24 +88,25 @@ declare global {
 
 export default function Home() {
   const [scoreObtained, setScore] = useState(0);
+  const [walletAddress, setWalletAddress] = useState();
+  const provider = useRef<BrowserProvider | null>(null);
+  const signer = useRef<JsonRpcSigner | null>(null);
 
   function updateScore() {
     let newVal = scoreObtained + 1;
     setScore(newVal);
   }
-  const [walletAddress, setWalletAddress] = useState();
 
-  async function backendFetch(address: any) {
-    try {
-      console.log("walletAddress before fetch:", address);
-      const data = await fetch(`/api/score?wallet=${address}`);
-      const json = await data.json();
-      console.log(json);
-      setScore(parseInt(json.score));
-    } catch (err) {
-      console.log("Error");
-    }
-  }
+  // async function backendFetch(address: any) {
+  //   try {
+  //     console.log("walletAddress before fetch:", address);
+  //     const data = await fetch(`/api/score?wallet=${address}`);
+  //     const json = await data.json();
+  //     setScore(parseInt(json.score));
+  //   } catch (err) {
+  //     console.log("Error");
+  //   }
+  // }
 
   const connectWallet = async () => {
     if (typeof window.ethereum !== "undefined") {
@@ -39,9 +116,26 @@ export default function Home() {
           method: "eth_requestAccounts",
         });
         setWalletAddress(accounts[0]);
-        console.log("Connected wallet:", accounts[0]);
-        backendFetch(accounts[0]);
-      } catch (err) {
+        provider.current = new ethers.BrowserProvider(window.ethereum);
+        signer.current = await provider.current!.getSigner();
+        if (contractAddr) {
+          const contract = new ethers.Contract(
+            contractAddr,
+            ABI,
+            signer.current
+          );
+          const tx = await contract.getData(accounts[0]);
+          await tx.wait();
+          const data = tx.toString();
+          setScore(data);
+        }
+
+        // backendFetch(accounts[0]);
+      } catch (err: any) {
+        if (err.code === "BAD_DATA" && err.value === "0x") {
+          console.warn("Could not decode data, defaulting to 0");
+          setScore(0);
+        }
         console.error("User denied wallet connection", err);
       }
     } else {
@@ -63,13 +157,12 @@ export default function Home() {
 
   async function postData() {
     try {
-      const res = await fetch("/api/score", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ score: scoreObtained }),
-      });
-      const data = await res.json();
-      console.log(data);
+      if (contractAddr) {
+        const contract = new ethers.Contract(contractAddr, ABI, signer.current);
+        const tx = await contract.setData(scoreObtained);
+        tx.await();
+        console.log("Save data successful");
+      }
     } catch (err) {
       console.log("Error in sending data");
     }
